ac4c13e852587314dbe8c93e5ba4e10ab9839a96
Noritaka Tasho <tasho@itccorporation.jp>
2021-05-17 23:03:33 +0900

    Supports GitHub Style markdown
    ReactMarkdownにremark-gfmプラグイン導入
    テーブルがGitHub StyleになるCSSを追加
    アンカーリンクのずれを調整するCSSを追加

--- components/Markdown.js
+++ components/Markdown.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Code from './Code';
 import ReactMarkdown from 'react-markdown/with-html';
 import { HeadingRenderer } from '../libs/Functions';
+import gfm from 'remark-gfm'
 
 export const Markdown = (props) => {
   return (
     <ReactMarkdown
       key="content"
+      plugins={[gfm]}
       source={props.source}
       renderers={{
         code: Code,heading: HeadingRenderer
